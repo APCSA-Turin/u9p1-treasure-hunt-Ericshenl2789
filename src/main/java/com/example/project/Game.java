23ba8c53b22159042d39b8acfda000f55fb14f0d
Eric Lin <ericshenl2789@nycstudents.net>
2025-03-28 13:10:37 +0000

    commit

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -10,6 +10,11 @@ public class Game{
     private int size; 
 
     public Game(int size){ //the constructor should call initialize() and play()
+        this.size = size;
+        enemies = new Enemy[2];
+        treasures = new Treasure[2];
+        initialize();
+        play();
     }
 
     public static void clearScreen() { //do not modify
@@ -31,7 +36,6 @@ public class Game{
     public void play(){ //write your game logic here
         Scanner scanner = new Scanner(System.in);
 
-
         while(true){
             try {
                 Thread.sleep(100); // Wait for 1/10 seconds
@@ -39,20 +43,59 @@ public class Game{
                 e.printStackTrace();
             }
             clearScreen(); // Clear the screen at the beggining of the while loop
-
-     
+            grid.display();
+            //coordinates, rowCol, treasure count and lives for debugging.
+            System.out.println(player.getCoords());
+            System.out.println(player.getRowCol(size));
+            System.out.println("Treasure: " + player.getTreasureCount());
+            System.out.println("Lives: " + player.getLives());
+            //userinput for direction.
+            String input = scanner.nextLine().toLowerCase();
+            if(input.equals("w")|| input.equals("s") || input.equals("a") || input.equals("d")){//if the key is correct, then player would move
+                //creating Sprite to represent the sprite that the player is going to land on
+                Sprite thing = new Sprite(0,0);
+                //converting x, y to row col to get the sprite from the grid
+                if(player.isValid(size, input)){
+                    thing = grid.getGrid()[size - 1 - (input.equals("w") ? player.getY() + 1 : input.equals("s") ? player.getY() - 1 : player.getY())][input.equals("d") ? player.getX() + 1 : input.equals("a") ? player.getX() - 1 : player.getX()];
+                }
+                //player actually moving
+                player.interact(size, input, treasures.length, thing);
+                if((!(thing instanceof Trophy)|| player.getTreasureCount() == treasures.length) && player.isValid(size, input)){
+                    player.move(input);
+                }
+                grid.placeSprite(player, input);
             }
+            if(player.getLives() == 0){
+                grid.gameover();
+                break;
+            } else if(player.getWin()){
+                grid.win();
+                break;
+            }
+        }
             
-     
     }
 
     public void initialize(){
-
         //to test, create a player, trophy, grid, treasure, and enemies. Then call placeSprite() to put them on the grid
-   
+        grid = new Grid(size);
+        player = new Player(0, 0);
+        grid.placeSprite(player);
+        Enemy enemy = new Enemy(5, 5);
+        Enemy enemy2 = new Enemy(7, 8);
+        grid.placeSprite(enemy2);grid.placeSprite(enemy);
+        enemies[0] = enemy;
+        enemies[1] =  enemy2;
+        Treasure treasure = new Treasure(2, 2);
+        Treasure treasure2 = new Treasure(1,7);
+        grid.placeSprite(treasure);grid.placeSprite(treasure2);
+        treasures[0] = treasure;
+        treasures[1] = treasure2;
+        trophy = new Trophy(9, 9);
+        grid.placeSprite(trophy);
     }
 
     public static void main(String[] args) {
-        
+        Game a = new Game(10);
     }
 }
--- src/main/java/com/example/project/Grid.java
+++ src/main/java/com/example/project/Grid.java
@@ -7,29 +7,86 @@ public class Grid{
     private int size;
 
     public Grid(int size) { //initialize and create a grid with all DOT objects
+        this.size = size;
+        grid = new Sprite[size][size];
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                grid[i][j] = new Dot(i, j);
+            }
+        }
     }
 
  
     public Sprite[][] getGrid(){return grid;}
 
-
-
     public void placeSprite(Sprite s){ //place sprite in new spot
-
+        grid[(size - 1) - s.getY()][s.getX()] = s;
     }
 
     public void placeSprite(Sprite s, String direction) { //place sprite in a new spot based on direction
-
+        Dot empty = new Dot(0, 0);
+        placeSprite(s);
+        if(direction.equals("w")){
+            empty.setY(s.getY() - 1);
+        } else if(direction.equals("s")){
+            empty.setY(s.getY() + 1);
+        } else {
+            empty.setY(s.getY());
+        }
+        if(direction.equals("a")){
+            empty.setX(s.getX() + 1 );
+        }else if(direction.equals("d")){
+            empty.setX(s.getX() - 1);
+        } else{
+            empty.setX(s.getX());
+        }
+        placeSprite(empty);
     }
 
 
     public void display() { //print out the current grid to the screen 
+        for(Sprite[] row : grid){
+            for(Sprite thing : row){
+                if(thing instanceof Dot){
+                    System.out.print("⬜");
+                } else if(thing instanceof Player){
+                    System.out.print("👾");
+                } else if (thing instanceof Enemy){
+                    System.out.print("🐲");
+                }else if(thing instanceof Treasure && !(thing instanceof Trophy)){
+                    System.out.print("🪙 ");
+                } else if(thing instanceof Trophy){
+                    System.out.print("🏆");
+                }
+            }
+            System.out.println();
+        }
     }
     
     public void gameover(){ //use this method to display a loss
+        for(Sprite[] row : grid){
+            for(Sprite thing : row){
+                if(!(thing instanceof Player)){
+                    System.out.print("❌");
+                } else if(thing instanceof Player){
+                    System.out.print("👾");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public void win(){ //use this method to display a win 
+        for(Sprite[] row : grid){
+            for(Sprite thing : row){
+                if(!(thing instanceof Player)){
+                    System.out.print("🟨");
+                } else if(thing instanceof Player){
+                    System.out.print("🌌");
+                }
+            }
+            System.out.println();
+        }
     }
 
 
